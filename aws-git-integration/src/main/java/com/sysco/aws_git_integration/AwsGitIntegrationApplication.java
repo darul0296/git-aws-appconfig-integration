e83befb0f5fcb60bf3467c178d0980e118f520e1
darul0296 <Daniel.Arul@sysco.com>
2024-06-12 13:11:45 +0530

    Initial Commit to implement AWS-Git Integration

---
+++ aws-git-integration/src/main/java/com/sysco/aws_git_integration/AwsGitIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.sysco.aws_git_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsGitIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsGitIntegrationApplication.class, args);
+	}
+
+}
---
+++ aws-git-integration/src/test/java/com/sysco/aws_git_integration/AwsGitIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysco.aws_git_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsGitIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
