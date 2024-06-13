609a410cb572b252f6ff41c1f2fb5831943e0379
darul0296 <Daniel.Arul@sysco.com>
2024-06-13 11:57:42 +0530

    Test Commit for aws-git-integration code pipleline

--- aws-git-integration/src/main/java/com/sysco/aws_git_integration/AwsGitIntegrationApplication.java
+++ aws-git-integration/src/main/java/com/sysco/aws_git_integration/AwsGitIntegrationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsGitIntegrationApplication {
 
+	//Test Commit
 	public static void main(String[] args) {
 		SpringApplication.run(AwsGitIntegrationApplication.class, args);
 	}
