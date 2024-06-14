e950607e9ab1d7320950071b47117d4cc25eec1a
darul0296 <Daniel.Arul@sysco.com>
2024-06-14 13:14:36 +0530

    Testing Lambda function

--- aws-git-integration/src/main/java/com/sysco/aws_git_integration/AwsGitIntegrationApplication.java
+++ aws-git-integration/src/main/java/com/sysco/aws_git_integration/AwsGitIntegrationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsGitIntegrationApplication {
 
-	//Test Commit3
+	//Test Commit4
 	public static void main(String[] args) {
 		SpringApplication.run(AwsGitIntegrationApplication.class, args);
 	}
